54f2fe4ac5555dddfe44228fc1da927d21eeef1c
Elisa Shapiro <elisashapiro@gmail.com>
2023-09-30 18:03:22 -0400

    tests passed

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci = []
+    if length > 0:
+        fibonacci.append(0)
+        if length > 1:
+            fibonacci.append(1)
+            if length > 2: 
+                i = 2
+                while i <length:
+                    fibonacci.append(
+                        fibonacci[i - 1] + fibonacci[i - 2])
+                    i += 1
+    print(fibonacci)
